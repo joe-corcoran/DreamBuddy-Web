b1b799a90962b50f731ed7646551d3f5e62756a5
Joe Corcoran <joecorcoran.com@gmail.com>
2024-11-25 12:28:20 -0500

    vite update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
         target: process.env.VITE_APP_API_URL || 'http://127.0.0.1:5000',
         changeOrigin: true,
         secure: false,
+        credentials: 'include'
       },
     },
   },
